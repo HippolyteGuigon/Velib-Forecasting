441ba0e661bb48b5c37a409ffe69487f9d14313b
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2024-04-05 11:41:15 +0200

    added setup config file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="velib_forecasting",
+    version="0.1.0",
+    packages=find_packages(
+        include=["velib_forecasting", "velib_forecasting.*"]
+    ),
+    description="Python programm for creating a forecasting\
+        program for velib consumption in Pparis",
+    author="Hippolyte Guigon",
+    author_email="Hippolyte.guigon@hec.edu",
+    url="https://github.com/HippolyteGuigon/Velib-Forecasting",
+)
