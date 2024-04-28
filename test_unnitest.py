96ba87f4bc56723bbd64bc145dc0ac42da6b86ec
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2024-04-28 18:09:30 +0200

    added unnitest structure with airflow first test

---
+++ test_unnitest.py
@@ -0,0 +1,47 @@
+import unittest
+import os
+
+from velib_forecasting.ETL.dags.airflow_dag import velib_station_info_pipeline
+from google.cloud import bigquery
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "velib-forecasting-auth.json"
+client = bigquery.Client()
+
+
+class Test(unittest.TestCase):
+    """
+    The goal of this class is to implement unnitest
+    and check everything commited makes sense
+    """
+
+    def test_velib_info_pipeline(self) -> None:
+        """
+        The goal of this function is to check
+        that the information about velib station
+        are well extracted and loaded on the BigQuery
+        table
+
+        Arguments:
+            -None
+        Returns:
+            -None
+        """
+
+        table_id = "velib-forecasting.velib_info.hourly_velib_places"
+        query = f"SELECT COUNT(*) AS total_rows FROM `{table_id}`"
+        query_job = client.query(query)
+
+        total_rows = list(query_job.result())[0]
+
+        velib_station_info_pipeline()
+
+        query_check = f"SELECT COUNT(*) AS total_rows FROM `{table_id}`"
+        query_job_check = client.query(query_check)
+
+        total_rows_check = list(query_job_check.result())[0]
+
+        self.assertGreater(total_rows_check, total_rows)
+
+
+if __name__ == "__main__":
+    unittest.main()
